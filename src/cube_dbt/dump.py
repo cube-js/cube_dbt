273b8d378c5940e8c3d639b7987d363b911da197
Igor Lukanin <me@igorlukanin.io>
2023-10-18 14:59:39 +0200

    feat: Protect escaping from Jinja for as_cube()

--- src/cube_dbt/dump.py
+++ src/cube_dbt/dump.py
@@ -1,9 +1,15 @@
 import yaml
 
+class SafeString(str):
+    is_safe: bool
+
+    def __init__(self, v: str):
+        self.is_safe = True
+
 class Dumper(yaml.Dumper):
   def increase_indent(self, flow=False, indentless=False):
     return super(Dumper, self).increase_indent(flow, indentless)
-    
+
 def indent_string(string: str, indent: int) -> str:
   return '\n'.join(
     (' ' * indent if i > 0 else '') +
@@ -19,4 +25,4 @@ def dump(data, indent: int=0) -> str:
     allow_unicode=True,
     indent=0
   )
-  return indent_string(dump, indent)
+  return SafeString(indent_string(dump, indent))
