fe187863c94d812c0f35b4906183f9c4949c8eee
samuelberntzen <samuel.berntzen@protonmail.com>
2024-03-23 12:53:05 +0100

    formatted file to original format

--- src/cube_dbt/dbt.py
+++ src/cube_dbt/dbt.py
@@ -1,18 +1,16 @@
 import json
 import locale
 from urllib.request import urlopen
-
 from cube_dbt.model import Model
-
-
+    
 class Dbt:
-    def __init__(self, manifest: dict) -> None:
-        self.manifest = manifest
-        self.paths = ""
-        self.tags = []
-        self.names = []
-        self._models = None
-        pass
+  def __init__(self, manifest: dict) -> None:
+    self.manifest = manifest
+    self.paths = ''
+    self.tags = []
+    self.names = []
+    self._models = None
+    pass
 
     @staticmethod
     def from_file(manifest_path: str, encoding: str = None) -> "Dbt":
@@ -45,36 +43,29 @@ class Dbt:
         with urlopen(manifest_url) as file:
             manifest = json.loads(file.read())
             return Dbt(manifest)
+    
+  def filter(self, paths: list[str]=[], tags: list[str]=[], names: list[str]=[]) -> 'Dbt':
+    self.paths = paths
+    self.tags = tags
+    self.names = names
+    return self
 
-    def filter(
-        self, paths: list[str] = [], tags: list[str] = [], names: list[str] = []
-    ) -> "Dbt":
-        self.paths = paths
-        self.tags = tags
-        self.names = names
-        return self
-
-    def _init_models(self):
-        if self._models == None:
-            self._models = list(
-                Model(node)
-                for key, node in self.manifest["nodes"].items()
-                if node["resource_type"] == "model"
-                and node["config"]["materialized"] != "ephemeral"
-                and (
-                    any(node["path"].startswith(path) for path in self.paths)
-                    if self.paths
-                    else True
-                )
-                and all(tag in node["config"]["tags"] for tag in self.tags)
-                and (node["name"] in self.names if self.names else True)
-            )
-
-    @property
-    def models(self) -> list[Model]:
-        self._init_models()
-        return self._models
-
-    def model(self, name: str) -> Model:
-        self._init_models()
-        return next(model for model in self._models if model.name == name)
+  def _init_models(self):
+    if self._models == None:
+      self._models = list(
+        Model(node) for key, node in self.manifest['nodes'].items()
+        if node['resource_type'] == 'model' and
+        node['config']['materialized'] != 'ephemeral' and
+        (any(node['path'].startswith(path) for path in self.paths) if self.paths else True) and
+        all(tag in node['config']['tags'] for tag in self.tags) and
+        (node['name'] in self.names if self.names else True)
+      )
+  
+  @property
+  def models(self) -> list[Model]:
+    self._init_models()
+    return self._models
+  
+  def model(self, name: str) -> Model:
+    self._init_models()
+    return next(model for model in self._models if model.name == name)
